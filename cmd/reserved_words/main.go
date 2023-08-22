621bfcf42453431e34b92dbdd0dcdf71c07a6af3
Stepan Filippov <stepan.filippov@yandex.ru>
2023-08-22 13:14:04 +0500

    changed source of generation

--- cmd/reserved_words/main.go
+++ cmd/reserved_words/main.go
@@ -54,7 +54,10 @@ func main() {
 
 	last := strings.SplitAfter(gramParts[1], "\n")
 	num = 1
+	old := make(map[string]struct{})
+	old[strings.Split(strings.Trim(last[0], "	 "), " {$$=$1}")[0]] = struct{}{}
 	for strings.Contains(last[num], "|") {
+		old[strings.Split(strings.Split(last[num], "| ")[1], " {$$=$1}")[0]] = struct{}{}
 		num++
 	}
 
@@ -65,35 +68,43 @@ func main() {
 		}
 		num++
 	}
+
+	fmt.Println("Removed: ")
+	for k := range old {
+		if _, ok := keyWords[k]; !ok {
+			fmt.Println(k)
+		}
+	}
+	fmt.Println("\nAdded: ")
+	for k := range keyWords {
+		if _, ok := old[k]; !ok {
+			fmt.Println(k)
+		}
+	}
 }
 
 func getReservedWords(directory string) map[string]struct{} {
-	b, err := ioutil.ReadFile(directory + "/lyx/lexer.rl")
+	b, err := ioutil.ReadFile(directory + "/lyx/gram.go")
 	if err != nil {
 		panic(err)
 	}
 
 	keyWords := make(map[string]struct{})
 
-	for i, el := range strings.Split(string(b), "\n") {
-		if i == 0 || strings.Contains(el, "#") {
+	temp := strings.Split(strings.Split(strings.Split(string(b), "var yyToknames =")[1], "{")[1], "}")[0]
+
+	for _, el := range strings.Split(temp, "\n") {
+		if strings.Contains(el, `//`) || len(el) < 3 {
 			continue
 		}
 
-		tem := strings.Split(el, "tok = ")
+		word := strings.Split(el, `"`)[1]
 
-		if len(tem) == 1 {
+		if strings.Compare(word, "$end") == 0 || strings.Compare(word, "error") == 0 || strings.Compare(word, "$unk") == 0 {
 			continue
-		} else if len(tem) > 2 {
-			panic(fmt.Errorf("more than one token in string"))
 		}
 
-		tok := strings.Split(tem[1], ";")
-		str := tok[0]
-		if strings.Contains(tok[0], "(") {
-			str = strings.ReplaceAll(strings.Split(tok[0], "(")[1], ")", "")
-		}
-		keyWords[str] = struct{}{}
+		keyWords[word] = struct{}{}
 	}
 
 	return keyWords
